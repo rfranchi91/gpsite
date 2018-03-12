14ad11f305da988ca6d33fd88980c9c0fac98997
giorgiauffre <33029145+giorgiauffre@users.noreply.github.com>
2018-03-12 11:51:17 +0100

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { Page3Component } from './pages/page3/page3.component';
+import { FooterComponent } from './extra/footer/footer.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component,
+    Page3Component,
+    FooterComponent,
+
+
   ],
   imports: [
     BrowserModule
