52eb2d3a4e9bb5227bc2a0b24c67199549110066
giorgiauffre <33029145+giorgiauffre@users.noreply.github.com>
2018-03-13 15:18:20 +0100

    ss

--- src/app/wwu/wwu.component.ts
+++ src/app/wwu/wwu.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
+import * as Resource from './../resource.json';
+
 @Component({
   selector: 'app-wwu',
   templateUrl: './wwu.component.html',
   styleUrls: ['./wwu.component.css']
 })
 export class WwuComponent implements OnInit {
+
+  resource:any = Resource;
 
   public nameStr: string;
   public emailStr: string;
@@ -19,7 +23,9 @@ export class WwuComponent implements OnInit {
   public searchParameter;
   
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
 
