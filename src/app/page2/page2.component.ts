68e4c3c5550353c8f53795b50bbe1bbb7612fcc8
Bonne <emanuele.bonne@github.com>
2018-03-14 17:28:20 +0100

    accordion page2

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -2,8 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import * as Resource from './../resource.json';
 
+import { trigger, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-page2',
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateY(-30%)', opacity: 0}),
+          animate('250ms', style({transform: 'translateY(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateY(0)', opacity: 1}),
+          animate('250ms', style({transform: 'translateY(-30%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
   templateUrl: './page2.component.html',
   styleUrls: ['./page2.component.css']
 })
@@ -21,10 +37,12 @@ export class Page2Component implements OnInit {
   private pdf: string = null;
   public loading: boolean = false;
 
+  public selectedAccordion: number = -1000;
+
   public opportunitiesSample = [
     {
     "title" : "Corso n.1",
-    "skills": ["programmer", "p1", "p2"]
+    "skills": ["Programmer", "1", "2"]
     },
     {
     "title" : "Corso n.2",
@@ -44,33 +62,44 @@ export class Page2Component implements OnInit {
 
   }
 
-    performFileInput() {
-      $("#fileInput").click();
-      //this.loading = true;
+  openAccordion(index : number) {
+    if(this.selectedAccordion == index) {
+      this.selectedAccordion = -1000;
+      //console.log(this.selectedAccordion);
+      return;
     }
-  
-    // Used to load a pdf
-    fileChangeListener($event) {
-      this.loading = true;
-      var that = this;
+    this.selectedAccordion = index;
+    //console.log(this.selectedAccordion);
+  }
 
-      this.pdf = null;
-      this.fileName = null;
-      console.log(this.loading);
+  // Used to link the Upload button to the file input button
+  performFileInput() {
+    $("#fileInput").click();
+    //this.loading = true;
+  }
 
-      let file: File = $event.target.files[0];
-      let myReader: FileReader = new FileReader();
+  // Used to load a pdf
+  fileChangeListener($event) {
+    this.loading = true;
+    var that = this;
 
-      myReader.readAsDataURL(file);
-      myReader.onloadend = function (loadEvent: any) {
-        that.pdf = loadEvent.target.result;
-        console.log(that.pdf);
-        that.fileName = file.name;
-        console.log(that.fileName);
-        that.fileSelected = true;
-        that.loading = false;
-        console.log(that.loading);
-      };      
-    }
+    this.pdf = null;
+    this.fileName = null;
+    console.log(this.loading);
+
+    let file: File = $event.target.files[0];
+    let myReader: FileReader = new FileReader();
+
+    myReader.readAsDataURL(file);
+    myReader.onloadend = function (loadEvent: any) {
+      that.pdf = loadEvent.target.result;
+      console.log(that.pdf);
+      that.fileName = file.name;
+      console.log(that.fileName);
+      that.fileSelected = true;
+      that.loading = false;
+      console.log(that.loading);
+    };      
+  }
 
 }
